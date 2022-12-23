3d996975ae52642669c03b8ef6ed9490fed3505c
Michael Evans <Michael.evans1775@outlook.com>
2022-12-23 16:38:28 -0700

    added twilio

--- venv/main.py
+++ venv/main.py
@@ -4,8 +4,22 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 
+# gather sensitive data
 load_dotenv("./vars/.env")
 sid = os.environ.get('SID')
-pokemon = pb.pokemon(random.randint(1,1008))
-print(pokemon.name)
+auth = os.environ.get('AUTH')
+nums = os.environ.get('myNumber')
+
+pokemon = pb.pokemon(random.randint(1, 1008))
+print(pokemon.abilities)
 print(sid)
+
+client = Client(sid, auth)
+message = client.messages.create(
+    to=nums,
+    from_= "19789042343",
+    body=pokemon.name
+
+)
+
+
